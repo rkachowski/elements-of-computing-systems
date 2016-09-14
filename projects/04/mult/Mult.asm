9a64313eed92120b18d5726dfbfc7a30beead5c2
Donald Hutchison <git@toastymofo.net>
2016-09-14 14:13:04 +0200

    multiply

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+(SETUP)
+    @count
+    M=0
+    @total
+    M=0
+(LOOP)
+    @count //check exit condition
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JEQ
+
+    @count //inc counter
+    M=M+1
+
+    @total //inc total
+    D=M
+    @R1
+    D=D+M
+    @total
+    M=D
+    
+
+    @LOOP
+    0;JMP
+
+(END)
+    @total
+    D=M
+    @R2
+    M=D
